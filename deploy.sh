018a89b8bb7b8a0adebe59101800b3ba34dd9818
podiukov.iv <ilia.podiukov@dom.ru>
2025-11-15 18:13:45 +0500

    upd sh

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,10 @@ docker rm nginx
 
 docker build -t nginx ./nginx
 
-docker run -d --name nginx -p 54321:80 
+docker run -d --name nginx -p 54321:80 nginx
 
 #проверка
 docker ps -a
+sleep 5
 curl 127.0.0.1:54321
+docker logs -n 10 nginx
